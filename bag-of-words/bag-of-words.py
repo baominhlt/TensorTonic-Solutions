a64e2f493b35eb3e0639b2c96fdc374b8bfdc723
baominhlt <98018730+baominhlt@users.noreply.github.com>
2026-04-03 14:06:36 +0700

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -4,6 +4,4 @@ def bag_of_words_vector(tokens, vocab):
     """
     Returns: np.ndarray of shape (len(vocab),), dtype=int
     """
-    if len(vocab) == 0:
-        return np.array([], dtype=int)
     return np.array([tokens.count(token) for token in vocab], dtype=int)
